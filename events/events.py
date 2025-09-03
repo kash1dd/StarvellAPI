dbe3f67c2da7bed890aa14499e2678cb9c7cedbe
Mark <163640647+kash1dd@users.noreply.github.com>
2025-09-04 00:37:15 +0300

    Док-стринги/Тайп-хинты
    Signed-off-by: Mark <kash1dmark@gmail.com>

--- events/events.py
+++ events/events.py
@@ -3,8 +3,19 @@ from StarvellAPI.socket import Socket
 
 class Runner:
     def __init__(self, acc: Account, always_online: bool = True):
-        self.acc = acc
-        self.socket = Socket(acc.session_id)
+        """
+        :param acc: Экземпляр класса Account
+        :param always_online: Поддерживать-ли постоянный онлайн? (Да - при использовании API, аккаунт всегда будет в онлайне)
+        """
 
-    def msg_handler(self, func):
+        self.acc = acc
+        self.socket = Socket(acc.session_id, always_online)
+
+    def msg_handler(self, func) -> None:
+        """
+        Добавляет хэндлер в обработчики класса Socket
+
+        :param func: Функция (хэндлер), которая будет обрабатывать ивент (Должна принимать только 1 аргумент - NewMessageEvent)
+        """
+
         self.socket.event_handlers.append(func)
--- socket.py
+++ socket.py
@@ -8,12 +8,24 @@ from StarvellAPI.common.enums import MessageTypes
 
 class Socket:
     def __init__(self, session_id: str, online: bool = True):
+        """
+        :param session_id: session_id со Starvell
+        :param online: Поддерживать-ли постоянный онлайн? (Да - при использовании API, аккаунт всегда будет в онлайне)
+        """
+
         self.s = session_id
         self.event_handlers = []
         self.online = online
         self.run_socket()
 
-    def on_message(self, ws: websocket.WebSocket, msg: str):
+    def on_message(self, ws: websocket.WebSocket, msg: str) -> None:
+        """
+        Вызывается при новом сообщении в веб сокете, и соответственно вызывает все привязанные хэндлеры
+
+        :param ws: Экземпляр класса WebSocket
+        :param msg: Сообщение веб сокета
+        """
+
         if msg.startswith('42/chats'):
             for event in self.event_handlers:
                 try:
@@ -31,13 +43,25 @@ class Socket:
         elif msg == "2":
             ws.send("3")
 
-    def on_open(self, ws: websocket.WebSocket):
+    def on_open(self, ws: websocket.WebSocket) -> None:
+        """
+        Вызывается при открытии веб сокета
+
+        :param ws: Экземпляр класса WebSocket
+        """
+
         ws.send("40/chats,")
 
         if self.online:
             ws.send('40/online,')
 
-    def init(self, **kwargs):
+    def init(self, **kwargs) -> None:
+        """
+        Запускает веб сокет
+
+        :param kwargs: Аргументы, которые можно указать в классе WebSocketApp (Не используется)
+        """
+
         url = "wss://starvell.com/socket.io/?EIO=4&transport=websocket"
         ws = websocket.WebSocketApp(
             url,
@@ -48,5 +72,9 @@ class Socket:
         )
         ws.run_forever(reconnect=True)
 
-    def run_socket(self):
+    def run_socket(self) -> None:
+        """
+        Запускает веб сокет в отдельном потоке
+        """
+
         threading.Thread(target=self.init).start()
