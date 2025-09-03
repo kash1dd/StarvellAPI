c14f597df390754d4b208d4f70184a723e00ef4b
Mark <163640647+kash1dd@users.noreply.github.com>
2025-09-04 00:39:27 +0300

    Удален менеджер потоков
    Signed-off-by: Mark <kash1dmark@gmail.com>

--- task_manager.py
+++
@@ -1,9 +0,0 @@
-import threading
-
-class TaskManager:
-    def __init__(self):
-        self.tasks: list[object] = []
-
-    def add_task(self, **kwargs):
-        task = threading.Thread(**kwargs).start()
-        self.tasks.append(task)
